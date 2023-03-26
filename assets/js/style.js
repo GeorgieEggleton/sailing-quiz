253bf9b912c2ef8aa5d09333c226d66059953038
GeorgieEggleton <georgie@sailingfaster.co.uk>
2023-03-26 17:19:51 +0000

    Add question counter and correct answer counter

--- assets/js/style.js
+++ assets/js/style.js
@@ -25,6 +25,70 @@ let questions = [
         option4: "White",
         answer: 2
     },
+
+     {
+        question: "When would you fly a spinnaker?", 
+        option1: "Sailing upwind",
+        option2: "Sailing downwind", 
+        option3: "Sailing across the wind",
+        option4: "When motoring",
+        answer: 2
+     },
+
+     {
+        question: "If you see a yacht displaying a black ball in the rigging what does this mean?", 
+        option1: "Sailing",
+        option2: "Adrift",
+        option3: "Anchored",
+        option4: "BBQing",
+        answer: 3
+     },
+
+     {
+        question: "Whilst at anchor what lights should a yacht display at night?", 
+        option1: "Red & green",
+        option2: "All round white", 
+        option3: "Flashing white",
+        option4: "Flashing red",
+        answer: 2
+     },
+
+     {
+        question: "You are in your boat and see a large container ship which give you 5 short blasts on their horn, what does this mean?",
+        option1: "They are turning to Port",
+        option2: "They are turning to Satrboard",
+        option3: "They would like you to turn to Starboard.", 
+        option4: "They are not sure of your intentions and concerned you are going to collide",
+        answer: 4
+     },
+
+     {
+        question: "What is a boat with one mast called?",
+        option1: "Sloop",
+        option2: "Ketch",
+        option3: "Schooner",
+        option4: "Yawl",
+        answer: 1
+     },
+
+     {
+        question: "If it is very windy you can make a boats sail smaller, what is this called?", 
+        options1: "Reefing",
+        option2: "Triming",
+        option3: "Tacking",
+        option4: "Winching",
+        answer: 1
+     },
+
+     {
+        question: "What is is called when a boat zig-zags in order to sail upwind?", 
+        option1: "Gybing",
+        option2: "Tacking",
+        option3: "Heeling over", 
+        option4: "Reefing",
+        answer: 2
+     }
+
 ];
 
 
@@ -39,6 +103,7 @@ let questionCount = 0;
 document.addEventListener("DOMContentLoaded", function() {
     
     questionNumber = getAQuestion();
+    incrementQuestions()
 
 
 
@@ -59,6 +124,7 @@ document.addEventListener("DOMContentLoaded", function() {
  
  function nextQuestion (){
     console.log("Question Count = " + questionCount); 
+    incrementQuestions()
     questionCount += 1;
     if(questionCount != 0){
       console.log("question Number= " + questionNumber);
@@ -93,7 +159,7 @@ document.addEventListener("DOMContentLoaded", function() {
     
                if (givenAnswer == questions[questionNumber].answer) {
                 alert ("Correct");
-                
+                incrementScore();
                 
                 setTimeout(nextQuestion, 1000);
                
@@ -103,9 +169,19 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert ("incorrect");
                 setTimeout(nextQuestion, 1000);
               
-              };
-                
-                
-            
+              };        
 };
+
+
+function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+}
+
+function incrementQuestions() {
+
+    let oldCount = parseInt(document.getElementById("question-counter").innerText);
+    document.getElementById("question-counter").innerText = ++oldCount;
+}
        
